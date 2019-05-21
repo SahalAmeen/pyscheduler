ebd12738ce26ae613913687f93890fdd1eb894d6
RumDaDuMCPE <ameensahal321@gmail.com>
2019-05-21 23:09:42 +0300

    Reducing repetition.

--- pySHIFT.py
+++ pySHIFT.py
@@ -11,7 +11,7 @@ CHANGELOG:
 """
 import os
 from itertools import permutations
-import mysql.connector as cnx
+from mysql import connector as cnx
 from random import randint
 
 # use mysql database for loading connection
@@ -39,41 +39,26 @@ except:  # TODO HANDLE EXCEPT
 cur.execute("USE my_db")
 
 # DECLARATION
-subjects = {
-    1: None,
-    2: None,
-    3: None,
-    4: None,
-    5: None,
-    6: None,
-    7: None,
-    8: None,
-    9: ["English", "Physics", "Chemistry", "Biology", "Maths"],
-    10: ["English", "Physics", "Chemistry", "Biology", "Maths"],
-    11: {'SCI': ["English", "M/C", "B/C", "Physics", "Chemistry"],
-         'COM': ["Subj1", "Subj2", "Subj3", "Subj4", "Subj5"],
-         'HUM': ["Subj1", "Subj2", "Subj3", "Subj4", "Subj5"]},
-    12: {'SCI': ["English", "M/C", "B/C", "Physics", "Chemistry"],
+''' Repetetion is bad. '''
+sub = ["English", "Physics", "Chemistry", "Biology", "Maths"]
+sub1 = {'SCI': ["English", "M/C", "B/C", "Physics", "Chemistry"],
          'COM': ["Subj1", "Subj2", "Subj3", "Subj4", "Subj5"],
          'HUM': ["Subj1", "Subj2", "Subj3", "Subj4", "Subj5"]}
-}
+subjects = {}
+for i in range(13):
+    if (i > 8) & (i < 11):
+        subjects[i] = sub
+    elif i > 10:
+        subjects[i] = sub1
+    else:
+        subjects[i] = None
+    
+    
 """
 # <editor-fold desc="Description">
-teachers = {
-    1: None,
-    2: None,
-    3: None,
-    4: None,
-    5: None,
-
-    6: None,
-    7: None,
-    8: None,
-    9: None,
-    10: None,
-    11: None,
-    12: None
-}
+teachers = {}
+for i in range(13):
+    teachers[i] = None
 # </editor-fold>"""  # in case of emergency
 
 
